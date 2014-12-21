bd57cc8c89cace5f9b848fabba28ffecb7cda53a
Darren Engwirda <d_engwirda@outlook.com>
2014-12-21 10:55:09 -0500

    Update drawtria2.m

--- drawtria2.m
+++ drawtria2.m
@@ -1,5 +1,12 @@
 function drawtria2(pp,tt,varargin)
 %DRAWTRIA2 draw a 2-simplex triangulation.
+%   DRAWTRIA2(PP,TT) draws the 2-simplex triangulation defi-
+%   ned by the nodes PP = [XI,YI,ZI] and triangles TT = [PI,
+%   PJ,PK].
+
+%   Darren Engwirda : 2014 --
+%   Email           : engwirda@mit.edu
+%   Last updated    : 01/12/2014
 
     fc = [.95,.95,.55];
     ec = [.15,.15,.15];
@@ -20,3 +27,4 @@ function drawtria2(pp,tt,varargin)
     if(~hs),hold off;end
     
 end
+
